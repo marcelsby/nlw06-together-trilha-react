19240fe937f0515e49003b733d8f3307518cd937
Marcel Barbosa <marcel.sby.br@gmail.com>
2021-06-21 19:41:45 -0300

    Learnt the fundamentals of React

--- letmeask/src/index.tsx
+++ letmeask/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Button } from './components/Button';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Button />
+    <Button />
+    <Button />
+    <Button />
   </React.StrictMode>,
   document.getElementById('root')
 );
